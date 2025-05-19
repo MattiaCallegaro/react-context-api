6f6085e130d1b6a254608090a679802181f45bff
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-05-19 16:01:02 +0200

    [UPD] Added context inside PostList

--- src/component/PostList.jsx
+++ src/component/PostList.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
+//importo il contesto
+//utilizzo il contesto
+import { useContext } from 'react'
+import PostContext from '../context/PostContext'
+import PostCard from './PostCard'
 
 const PostList = () => {
+
+    //useContext mi fa leggere i dati del contesto
+    const posts = useContext(PostContext)
     return (
         <div>PostList</div>
     )
