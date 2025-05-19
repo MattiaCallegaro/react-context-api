977b3c0ec3fbc94c616f52f9037b60dac0356058
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-05-19 15:39:58 +0200

    [UPD] Add Provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
 import posts from '../data/posts'
+import PostPage from './pages/PostPage'
+import PostContext from './context/PostContext'
+
 
 function App() {
 
 
   return (
     <>
+      {/* `rendo disponibili i post via Context */}
+      <PostContext.Provider value={posts}>
+        <PostPage />
+      </PostContext.Provider>
 
     </>
   )
